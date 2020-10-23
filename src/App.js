6be3826add1e7475a7f04d447ecbfcf6bd8cb891
markopedu <69790644+markopedu@users.noreply.github.com>
2020-10-23 16:11:15 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useMachine } from '@xstate/react';
+import { toggleMachine } from './toggleMachine';
 
 function App() {
+    const [ current, send ] = useMachine(toggleMachine);
+
+    console.log(current);
+    const styles = current.value === 'active' ? { background: 'red' } : { background: 'yellow' };
+
   return (
-    <div className="App">
-      <header className="App-header">
+    <div  className="App">
+      <header style={styles} className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <hr style={{width:'100%'}} />
+        <button style={{fontSize: '36px'}} onClick={() => send('TOGGLE')}>Toggle</button>
       </header>
     </div>
   );
---
+++ src/toggleMachine.js
@@ -0,0 +1,15 @@
+import { Machine } from 'xstate';
+
+
+export const toggleMachine = Machine({
+    id: 'toggle',
+    initial: 'inactive',
+    states: {
+        inactive: {
+            on: { TOGGLE: 'active' }
+        },
+        active: {
+            on: { TOGGLE: 'inactive' }
+        }
+    }
+});
